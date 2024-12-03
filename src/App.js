60864218160b5ac9d4c44e82844421dec000a839
3jfmin <185140062+3jfmin@users.noreply.github.com>
2024-12-03 11:10:41 +0900

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to EP Chat Tool</h1>
+      <p>Firebase is successfully configured!</p>
+    </div>
+  );
+}
+
+export default App;
